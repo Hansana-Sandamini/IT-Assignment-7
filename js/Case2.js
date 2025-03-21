99f944184f7a6a3f8e4de533e8fd8707b5b9b24e
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-03-21 11:45:45 +0530

    Added Case2.js file

---
+++ js/Case2.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    const boxes = $(".box").toArray();
+    let colors = boxes.map((box) => $(box).css("background-color"));
+  
+    function startAnimation() {
+      setInterval(() => {
+        const lastColor = colors.pop();
+        colors.unshift(lastColor);
+  
+        boxes.forEach((box, index) => {
+          $(box).css("background-color", colors[index]);
+          console.log(index);
+        });
+      }, 500);
+    }
+  
+    startAnimation();
+  });
